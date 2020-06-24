2f755ee67eeea31a30df1df0cc6b35e521ee4264
polakmosh <43119773+polakmosh@users.noreply.github.com>
2020-06-24 17:50:38 -0400

    starting loop from 2

--- 1/solution/prime.py
+++ 1/solution/prime.py
@@ -5,7 +5,7 @@ def is_prime(n):
     """
     Very slow implementation
     """
-    for i in range(1, n):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
